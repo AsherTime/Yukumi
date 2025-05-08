65c3191aa2b01497b3b6f999ed69ebeebe6f7002
Sarthak-Pattnaik <psarthak@protonmail.com>
2025-05-09 00:40:29 +0530

    Save local changes to next.config.js

--- next.config.js
+++ next.config.js
@@ -41,7 +41,12 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'hebbkx1anhila5yf.public.blob.vercel-storage.com',
         pathname: '/**',
-}
+      },
+      {
+        protocol: 'https',
+        hostname: 's4.anilist.co',
+        pathname: '/**',
+      }
     ],
   },
 }
