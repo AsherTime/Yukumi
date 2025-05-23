60f1c564271b944b72aa47c1b88e05d996e6feeb
Sarthak-Pattnaik <psarthak@protonmail.com>
2025-05-23 16:54:39 +0530

    Final commit of all remaining changes

--- src/components/logout.tsx
+++ src/components/logout.tsx
@@ -23,6 +23,7 @@ const LogoutButton = () => {
     return (
         <button
             onClick={handleLogout}
+            data-logout-button
             className="px-4 py-2 bg-red-600 text-white rounded-lg hover:bg-red-700 transition-colors"
         >
             Logout
